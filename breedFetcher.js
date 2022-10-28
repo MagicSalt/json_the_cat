69b0dbfbc1a416f000cf999b58cf27da499fb6c1
Darren Lin <darrenlin815@hotmail.com>
2022-10-27 18:22:59 -0700

    Refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,20 @@
 const request = require('request');
-const domain = 'https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2];
 
-request(domain, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log(error);
+const fetchBreedDescription = function(breed, callback) {
+  const domain = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(domain, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (data.length === 0) {
+      callback(`Couldn't find the specified breed`, null);
+      return;
+    }
+    callback(null, data[0].description);
     return;
-  }
-  if (data.length === 0) {
-    console.log(`Couldn't find the specified breed`);
-    return;
-  }
-  console.log(data[0].description);
-  return;
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
